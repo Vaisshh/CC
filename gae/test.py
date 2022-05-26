6b4d3a2efe596f5987e0233d72c3a5b018a678d2
Vaisshh <85280558+Vaisshh@users.noreply.github.com>
2022-05-26 07:25:55 +0530

    Add files via upload

---
+++ gae/test.py
@@ -0,0 +1,8 @@
+from distutils.log import debug
+import webapp2
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.writer("Hello World")
+
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
